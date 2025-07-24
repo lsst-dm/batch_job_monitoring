e1e438c232fdb1a48b883537ddcc34178b92cc4c
Kenneth R. Herner <kherner@lsst.org>
2025-07-24 11:49:15 -0700

    formatting fixes

--- bin.src/htcondor/htcondor_queries.py
+++ bin.src/htcondor/htcondor_queries.py
@@ -4,9 +4,9 @@ import time
 import datetime
 import json
 import os
-import classad
 import htcondor
 from opensearchpy import OpenSearch
+
 
 def configure_htcondor():
     htcondor.param['SEC_CLIENT_AUTHENTICATION_METHODS'] = 'SSL FS'
@@ -17,15 +17,15 @@ def configure_htcondor():
 
 # sampling interval in seconds
 sample_interval = 15
-# max run time in seconds. So sample every sample_interval seconds up to max_run.
+# max run time in seconds. Sample every sample_interval seconds up to max_run.
 max_run = 60
 debug = False
 host = "usdf-opensearch.slac.stanford.edu"
 port = 443
 auth = ("condor", os.environ['OSPWD'])
-client = OpenSearch(hosts = [{'host': host, "port": port}],http_compress= True,
-http_auth=auth, use_ssl = True, verify_certs = True, ssl_assert_hostname= False,
-ssl_show_warn = False)
+client = OpenSearch(hosts=[{'host': host, "port": port}], http_compress=True,
+                    http_auth=auth, use_ssl=True, verify_certs=True,
+                    ssl_assert_hostname=False, ssl_show_warn=False)
 
 os_index_name = 'htcondor-dev-totals-v2'
 
@@ -46,19 +46,17 @@ run_constraint = 'JobStatus==2&&JobUniverse!=7'
 idle_constraint = 'JobStatus==1&&JobUniverse!=7'
 dagman_constraint = 'JobUniverse!=7'
 
-stats = ["ClusterId","ProcId","JobStatus","JobUniverse",
-               "QDate","ServerTime","JobCurrentStartDate","RemoteUserCpu",
-               "EnteredCurrentStatus"]
-extra_stats=[
-    "Owner","AccountingGroup", "CpusProvisioned","NumRestarts",
-    "NumJobStarts","RemoteUserCpu", "RequestMemory",
-    "RequestDisk", "RequestCpus", "ResidentSetSize_RAW",
-    "DiskUsage_RAW", "HoldReason", "HoldReasonCode",
-    "HoldReasonSubcode", "bps_job_name", "bps_job_label",
-    "bps_campaign", "bps_job_quanta", "bps_job_summary",
-    "bps_operator", "bps_payload", "bps_project",
-    "bps_run", "bps_run_quanta"
-    ]
+stats = ["ClusterId", "ProcId", "JobStatus", "JobUniverse",
+         "QDate", "ServerTime", "JobCurrentStartDate",
+         "RemoteUserCpu", "EnteredCurrentStatus"]
+extra_stats = ["Owner", "AccountingGroup", "CpusProvisioned",
+               "NumRestarts", "NumJobStarts", "RemoteUserCpu",
+               "RequestMemory", "RequestDisk", "RequestCpus",
+               "ResidentSetSize_RAW", "DiskUsage_RAW", "HoldReason",
+               "HoldReasonCode", "HoldReasonSubcode", "bps_job_name",
+               "bps_job_label", "bps_campaign", "bps_job_quanta",
+               "bps_job_summary", "bps_operator", "bps_payload",
+               "bps_project", "bps_run", "bps_run_quanta"]
 
 
 start_time = time.time()
@@ -69,7 +67,7 @@ totals = {}
 totals['timestamp'] = int(start_time)
 
 jobkeys = ["Idle", "Running", "Held", "TotalCpus", "TotalRemoteUserCpu",
-           "TotalWallTime", "WeightedCpus", "Efficiency"]           
+           "TotalWallTime", "WeightedCpus", "Efficiency"]
 
 tprev = start_time
 while time.time() < start_time + max_run:
@@ -78,7 +76,7 @@ while time.time() < start_time + max_run:
     if tnow + sample_interval > start_time + max_run:
         break
     # if it isn't time to sample again yet, wait until it is
-    if tnow < tprev + sample_interval and nsamples>0:
+    if tnow < tprev + sample_interval and nsamples > 0:
         time.sleep(tprev + sample_interval - tnow)
     tprev = time.time()
 
@@ -94,9 +92,8 @@ while time.time() < start_time + max_run:
         # add stuff for other sites here down the road
 
         for ijob in jobs:
-            #convert to json
+            # convert to json
             jobjson = json.loads(ijob.printJson())
-            # if bps_runsite not the in job classads, attempt to infer from hostname
             if sitename not in totals.keys():
                 totals[sitename] = {}
             if jobjson['Owner'] not in totals[sitename].keys():
@@ -112,8 +109,8 @@ while time.time() < start_time + max_run:
                 activedict = totals[sitename][jobjson['Owner']][jobjson['bps_run']][jobjson['bps_job_label']]
                 for jkey in jobkeys:
                     activedict[jkey] = 0
-            activedict = totals[sitename][jobjson['Owner']][jobjson['bps_run']][jobjson['bps_job_label']]  
-            
+            activedict = totals[sitename][jobjson['Owner']][jobjson['bps_run']][jobjson['bps_job_label']]
+
             if jobjson['JobStatus'] == 1:
                 activedict['Idle'] += 1
             elif jobjson['JobStatus'] == 2:
@@ -126,11 +123,12 @@ while time.time() < start_time + max_run:
                 activedict['TotalCpus'] += ncpu
                 try:
                     activedict['WeightedCpus'] += max(jobjson['RequestMemory']/memory_slot, ncpu)
-                except:
-                    #try to fix it by forcing an evaluation of the RequestMemory classad of the job
+                except Exception:
+                    # try to fix it by forcing an evaluation of
+                    # the RequestMemory classad of the job
                     try:
                         activedict['WeightedCpus'] += max(ijob.eval("RequestMemory")/memory_slot, ncpu)
-                    except:
+                    except Exception:
                         print(f"{start_time}: Still a problem with WeightedCpus")
                         activedict['WeightedCpus'] += ncpu
                 activedict['TotalRemoteUserCpu'] += jobjson['RemoteUserCpu']
@@ -139,12 +137,14 @@ while time.time() < start_time + max_run:
                 activedict['Held'] += 1
     nsamples += 1
 
-# Now we have the json, let's push the record to the index, if we actually found anything
+# Now we have the json, let's push the record to the index,
+# if we actually found anything
 
 nsamples = max(nsamples, 1)
 
 for totkey, totval in totals.items():
-    # we are out of the while loop, so we have to divide the totals by the number of samples
+    # we are out of the while loop, so we have to divide the totals
+    # by the number of samples.
     if totkey != "timestamp":
         for okey, oval in totval.items():
             for bkey, bval in oval.items():
@@ -169,6 +169,7 @@ if debug:
 try:
     for idict in dictlist:
         result = client.index(index=os_index_name, body=idict)
-except:
+except Exception:
     logtime = datetime.datetime.fromtimestamp(start_time)
     print(f"{logtime}: Error uploading json to {os_index_name}.")
+    raise
